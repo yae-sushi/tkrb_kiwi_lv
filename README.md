77177e37d6d57e7c0af041bccf470a9fa9652077
Lucy <64810654+yae-sushi@users.noreply.github.com>
2026-08-18 14:19:43 -0400

    updated with new data (tsurugiwame, 199 level cap, etc). thanks crow!

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 ## About:
 
-This is a Streamlit app that informs the user what level their sword character will return as from kiwame training in the game [Touken Ranbu ONLINE (aka TKRB)](https://www.johren.net/games/tohken-en/play/).
+This is a Streamlit app that informs the user what level their sword character will return as from kiwame training in the game [Touken Ranbu ONLINE (aka TKRB)](https://games.dmm.com/detail/tohken/). RIP EN version.
 
 It can be used in two ways:
 
-- through showing how much exp is remaining for characters below level 99 until their next level, or
-- by inputting the cumulative EXP an untrained character currently has (given the game's recent UI changes allowing users to see "extra" EXP, as well as the ability for untrained swords to store "extra" EXP at level 99).
+- through showing how much exp is remaining for characters below max level until their next level, or
+- by inputting the cumulative EXP a character currently has (given the game's recent UI changes allowing users to see "extra" EXP, as well as the ability for untrained swords to store "extra" EXP at level 99).
 
 The app can be accessed here: https://share.streamlit.io/yae-sushi/tkrb_kiwi_lv/main/kiwame_return_calc.py. All written for fun.
 
@@ -15,12 +15,13 @@ The app can be accessed here: https://share.streamlit.io/yae-sushi/tkrb_kiwi_lv/
 
 - environment.yml - Tells Streamlit what Conda/Python environment requirements are needed for the app to run.
 - kiwame_return_calc.py - The actual calculator and Streamlit application.
-- tkrb_exp_scraper.ipynb - The code used to initially scrape information from TKRB's wikia. Also includes some data visualizations to satisfy my curiosity.
-- tkrb_exp.csv - The cleaned and scraped data produced by the scraper file, used by the calculator.
+- tkrb_exp_scraper.ipynb - (outdated, included for comparison) The code used to initially scrape information from TKRB's wikia. Also includes some data visualizations to satisfy my curiosity.
+- tkrb_exp_exploration.ipynb - The code used to clean/reorganize the initial csv file given to me by my friend Crow. Also includes some data visualizations to satisfy my curiosity. Initial CSV file not included.
+- tkrb_exp.csv - The clean data, used by the calculator.
 
 ## Attributions:
 
-The information required to make this calculator was scraped via BeautifulSoup from the Touken Ranbu wikia located at https://touken-ranbu.fandom.com/, and is included in .csv form (tkrb_exp.csv) in this repository. Any changes to the wikia since the scrape are not reflected in the data.
+The information required to make this calculator was given to me by my friend Crow, and after cleaning and reorganization is included in .csv form (tkrb_exp.csv) in this repository. Any changes to the game since are not reflected in the data.
 
 ## Other:
 
--- kiwame_return_calc.py
+++ kiwame_return_calc.py
@@ -9,7 +9,7 @@ KIWI_REQUIREMENTS = {
     "R3 Uchiwame" : 70,
     "R4 Uchiwame" : 70,
     "Tachiwame" : 75,
-    "Ootachiwame" : 80,
+    "Ootachi, Tsurugiwame" : 80,
 }
 
 TYPE_TO_CATEGORY = {
@@ -18,10 +18,10 @@ TYPE_TO_CATEGORY = {
     "Uchigatana (Current Rarity 2: Silver Crest)" : "R3 Uchiwame",
     "Uchigatana (Current Rarity 3: Gold Crest)" : "R4 Uchiwame",
     "Tachi" : "Tachiwame",
-    "Ootachi" : "Ootachiwame",
+    "Ootachi" : "Ootachi, Tsurugiwame",
     "Yari" : "Waki, Yari, Nagiwame",
     "Naginata" : "Waki, Yari, Nagiwame",
-    "Tsurugi" : "Tsurugiwame",
+    "Tsurugi" : "Ootachi, Tsurugiwame",
 }
 
 def calc_extra_exp(cur_lv, sword_type, exp, is_cum_exp):
@@ -68,8 +68,6 @@ def return_level(cur_lv, sword_type, exp = -1, is_cum_exp = False):
     Returns:
     int - level of kiwi toudan expected
     """
-    if sword_type == "Tsurugiwame":
-        sword_type = "Ootachiwame"
 
     extra_exp = calc_extra_exp(cur_lv, sword_type, exp, is_cum_exp)
     if extra_exp == 0:
@@ -124,15 +122,13 @@ def validate_exp_remaining(exp_remaining, is_cum_exp):
 st.title("What level kiwame will my touken danshi be?")
 
 st.write("""Let's find out! (The data needed to perform these calculations
-was scraped from the TKRB Fandom website at
-https://touken-ranbu.fandom.com/)""")
+was given to me by a friend.""")
 
 sword_type = TYPE_TO_CATEGORY[st.radio(
     "Sword type?",
     list(TYPE_TO_CATEGORY.keys()))]
 
-is_cum_exp = st.radio("""**NEW:** I can now calculate the return level based on a sword's cumulative EXP! Would you like to use this option?
-                        \n([How do I find the cumulative EXP?](https://i.imgur.com/PgU2Ws7.png))""",
+is_cum_exp = st.radio("""Calculate the return level based on a sword's cumulative EXP?""",
             ["Yes",
             "No"])
 
@@ -149,10 +145,7 @@ if is_cum_exp:
     cur_lv = -1
 
 else:
-    if sword_type != "Tsurugiwame":
-        start_slider_lv = KIWI_REQUIREMENTS[sword_type]
-    else:
-        start_slider_lv = KIWI_REQUIREMENTS["Ootachiwame"]
+    start_slider_lv = KIWI_REQUIREMENTS[sword_type]
 
     cur_lv = st.slider("Current toudan level?", 1, 99,
                         start_slider_lv)
@@ -178,21 +171,13 @@ if exp == None:
     else:
         st.write(f"{original_input} is not a valid answer. Please try again.")
 else:
-    tsurugi_text = "No tsurugi currently has a kiwame form. :(  \n\
-                    However, if we assume that they'll follow the same exp requirements \
-                    as ootachis...  \n"
+
     prediction = return_level(int(cur_lv), sword_type, int(exp), is_cum_exp)
     if prediction:
         output_text = f"Your sword will return as a :sparkles: level {prediction} kiwame :sparkles:."
     else:
-        if sword_type == "Tsurugiwame":
-            temp_sword_type = "Ootachiwame"
-        else:
-            temp_sword_type = sword_type
         output_text = f"Your sword is currently unable to kiwame. \
-            To do so, he must be level {KIWI_REQUIREMENTS[temp_sword_type]} or \
+            To do so, he must be level {KIWI_REQUIREMENTS[sword_type]} or \
             higher."
 
-    if sword_type == "Tsurugiwame":
-        output_text = tsurugi_text + output_text
     st.write(output_text)
